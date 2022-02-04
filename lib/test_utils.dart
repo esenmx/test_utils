cecc15034959a6c3ab9985058786c6f9e97a0eda
softronaut <mathronaut@gmail.com>
2022-02-04 17:19:26 +0300

    initial commit

---
+++ lib/test_utils.dart
@@ -0,0 +1,8 @@
+library test_utils;
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+part 'src/listener.dart';
+part 'src/widget.dart';
