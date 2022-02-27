5d1e5f4ffc0d236295c30165507b6d4c7c92ddf7
softronaut <mathronaut@gmail.com>
2022-02-27 16:17:07 +0300

    range naming tweaks, pubspec updates

--- lib/src/range.dart
+++ lib/src/range.dart
@@ -1,43 +1,43 @@
 part of test_utils;
 
-extension RangeIntExtensions on int {
+extension RangeIntEx on int {
   /// [0, ... , length - 1]
-  List<int> rangeSeq([int? offset]) {
+  List<int> range([int? offset]) {
     return List.generate(this, (index) => index + (offset ?? 0));
   }
 
   /// [0, 2, 4, ... , (length - 1) * 2]
-  List<int> rangeEvenSeq([int? offset]) {
+  List<int> rangeEven([int? offset]) {
     return List.generate(this, (index) => index * 2 + (offset ?? 0));
   }
 
   /// [1, 3, 5, ... , ((length + 1) * 2) - 1]
-  List<int> rangeOddSeq([int? offset]) {
+  List<int> rangeOdd([int? offset]) {
     return List.generate(this, (index) => index * 2 + 1 + (offset ?? 0));
   }
 
   /// {Random().nextInt(), Random().nextInt(), ... , Random().nextInt()};
-  List<int> rangeRandom([int? max]) {
+  List<int> rangeRand([int? max]) {
     return List.generate(this, (index) => Random().nextInt(max ?? this));
   }
 
   /// {0: true, 1: true, ... , (length - 1): true};
-  List<MapEntry<int, bool?>> rangeSeqEntries([bool? value = true]) {
-    return rangeSeq(this).map((e) => MapEntry(e, true)).toList();
+  List<MapEntry<int, bool?>> rangeEntry([bool? value = true]) {
+    return range(this).map((e) => MapEntry(e, true)).toList();
   }
 
   /// {0: true, 2: true, ... , ((length - 1) * 2): true};
-  List<MapEntry<int, bool?>> rangeEvenSeqEntries([bool? value = true]) {
-    return rangeEvenSeq(this).map((e) => MapEntry(e, value)).toList();
+  List<MapEntry<int, bool?>> rangeEntryEven([bool? value = true]) {
+    return rangeEven(this).map((e) => MapEntry(e, value)).toList();
   }
 
   /// {1: true, 3: true, ... , (((length + 1) * 2) - 1): true};
-  List<MapEntry<int, bool?>> rangeOddEntries([bool? value = true]) {
-    return rangeOddSeq(this).map((e) => MapEntry(e, value)).toList();
+  List<MapEntry<int, bool?>> rangeEntryOdd([bool? value = true]) {
+    return rangeOdd(this).map((e) => MapEntry(e, value)).toList();
   }
 
   /// {Random().nextInt(): true, ... , Random().nextInt(): true};
-  List<MapEntry<int, bool?>> rangeRandomEntries([bool? value = true]) {
-    return rangeRandom(this).map((e) => MapEntry(e, value)).toList();
+  List<MapEntry<int, bool?>> rangeEntryRand([bool? value = true]) {
+    return rangeRand(this).map((e) => MapEntry(e, value)).toList();
   }
 }
--- lib/src/widget.dart
+++
@@ -1,9 +0,0 @@
-// part of test_utils;
-//
-// T findWidget<T extends Widget>() {
-//   return find.byType(T).evaluate().first.widget as T;
-// }
-//
-// T findWidgetAt<T extends Widget>(int index) {
-//   return find.byType(T).evaluate().elementAt(index).widget as T;
-// }
