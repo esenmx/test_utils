401aa9fbd65bd000bc7f69c0c00c6d503d0e11cd
esenmx <mehmetesen@protonmail.com>
2022-06-17 11:14:26 +0300

    dependency bump

--- lib/test_utils.dart
+++ lib/test_utils.dart
@@ -6,7 +6,7 @@ import 'package:mockito/mockito.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:test/test.dart';
 
-part 'src/checker.dart';
 part 'src/listener.dart';
+part 'src/matcher.dart';
 part 'src/range.dart';
 part 'src/riverpod.dart';
